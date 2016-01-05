5160f3f834d1c4d100e29070be580f22cfef1eaa
dawnfh <2014fed@gmail.com>
2016-01-05 16:08:08 -0500

    add routes, user table

---
+++ models.rb
@@ -0,0 +1,15 @@
+class User <ActiveRecord::Base
+	has_many :posts
+	
+	validates_length_of :password, minimum: 5
+end
+
+class Post <ActiveRecord::Base
+		belongs_to :user
+end
+
+class Follow < ActiveRecord::Base
+ 
+  belongs_to :follower, foreign_key: :follower_id, class_name: User
+  belongs_to :followee, foreign_key: :followee_id, class_name: User
+end
