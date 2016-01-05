03e1651af8d1ea036687f5fc540706b20fdadc2a
Sarah Corey <syncorey@gmail.com>
2016-01-05 18:48:20 -0500

    added 'email' column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160105213017) do
+ActiveRecord::Schema.define(version: 20160105231502) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
@@ -23,12 +22,11 @@ ActiveRecord::Schema.define(version: 20160105213017) do
     t.string  "body"
     t.integer "user_id"
   end
-# ActiveRecord::Schema.define(version: 20160105202333) do
-
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "email"
   end
 
 end
